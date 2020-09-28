5848d2230cec7fd39aacb4d05df21a23f47a6b56
d175fleet <d175sfy@costco.com>
2020-09-28 04:49:49 -0400

    deleted CForm from ShowWet & ShowDryModal

--- src/views/dispatch/modal/wet/ShowWetModal.js
+++ src/views/dispatch/modal/wet/ShowWetModal.js
@@ -23,58 +23,7 @@ return (
       <CModalHeader closeButton>265 (Wet Loads)</CModalHeader>
       <CModalBody>
         <h3>265 (Wet Loads)</h3>
-      <CForm action="" method="post">
-            <CFormGroup>
-              <CLabel htmlFor="nf-whse">Whse</CLabel>
-              <CInput
-                type="number"
-                id="nf-number"
-                name="nf-number"
-                placeholder="Enter Whse.."
-              />
-              <CFormText className="help-block">WHSE</CFormText>
-            </CFormGroup>
-            <CFormGroup>
-              <CLabel htmlFor="nf-date">Date</CLabel>
-              <CInput
-                type="date"
-                id="nf-date"
-                name="nf-date"
-                placeholder="Enter Date.."
-              />
-              <CFormText className="help-block">DATE</CFormText>
-            </CFormGroup>
-            <CFormGroup>
-              <CLabel htmlFor="nf-appt">Appt. Time</CLabel>
-              <CInput
-                type="time"
-                id="nf-appt"
-                name="nf-appt"
-                placeholder="Enter Appt Time.."
-              />
-              <CFormText className="help-block">TIME</CFormText>
-            </CFormGroup>
-            <CFormGroup>
-              <CLabel htmlFor="nf-LoadNumber">Load Number</CLabel>
-              <CInput
-                type="number"
-                id="nf-loadNumber"
-                name="nf-loadNumber"
-                placeholder="LOAD NUMBER.."
-              />
-              <CFormText className="help-block">LOAD NUMBER</CFormText>
-            </CFormGroup>
-            <CFormGroup>
-              <CLabel htmlFor="nf-trailerNumber">Trailer Number</CLabel>
-              <CInput
-                type="trailerNumber"
-                id="nf-trailerNumber"
-                name="nf-trailerNumber"
-                placeholder="TRAILER NUMBER.."
-              />
-              <CFormText className="help-block">TRAILER NUMBER</CFormText>
-            </CFormGroup>
-          </CForm>
+
       </CModalBody>
       <CModalFooter>
         <CButton color="primary">Do Something</CButton>{' '}
