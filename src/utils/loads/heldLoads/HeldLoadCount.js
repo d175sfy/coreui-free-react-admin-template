39db6df043c9052addb9f7243ff1275ae89aa608
Eric Velez <d175sfy@costco.com>
2020-10-15 13:48:11 -0400

    added heldLoadCount

---
+++ src/utils/loads/heldLoads/HeldLoadCount.js
@@ -0,0 +1,48 @@
+
+
+//Held loads
+
+const Whse_222_held = HeldLoads.filter(load => load.Wets === 222).length
+const Whse_226_held = HeldLoads.filter(load => load.Wets === 226).length
+const Whse_230_held = HeldLoads.filter(load => load.Wets === 230).length
+const Whse_237_held = HeldLoads.filter(load => load.Wets === 237).length
+const Whse_240_held = HeldLoads.filter(load => load.Wets === 240).length
+const Whse_241_held = HeldLoads.filter(load => load.Wets === 241).length
+const Whse_243_held = HeldLoads.filter(load => load.Wets === 243).length
+const Whse_244_held = HeldLoads.filter(load => load.Wets === 244).length
+const Whse_245_held = HeldLoads.filter(load => load.Wets === 245).length
+const Whse_246_held = HeldLoads.filter(load => load.Wets === 246).length
+const Whse_248_held = HeldLoads.filter(load => load.Wets === 248).length
+const Whse_305_held = HeldLoads.filter(load => load.Wets === 305).length
+const Whse_306_held = HeldLoads.filter(load => load.Wets === 306).length
+const Whse_310_held = HeldLoads.filter(load => load.Wets === 310).length
+const Whse_315_held = HeldLoads.filter(load => load.Wets === 315).length
+const Whse_316_held = HeldLoads.filter(load => load.Wets === 316).length
+const Whse_318_held = HeldLoads.filter(load => load.Wets === 318).length
+const Whse_320_held = HeldLoads.filter(load => load.Wets === 320).length
+const Whse_321_held = HeldLoads.filter(load => load.Wets === 321).length
+const Whse_322_held = HeldLoads.filter(load => load.Wets === 322).length
+const Whse_323_held = HeldLoads.filter(load => load.Wets === 323).length
+const Whse_324_held = HeldLoads.filter(load => load.Wets === 324).length
+const Whse_326_held = HeldLoads.filter(load => load.Wets === 326).length
+const Whse_329_held = HeldLoads.filter(load => load.Wets === 329).length
+const Whse_729_held = HeldLoads.filter(load => load.Wets === 729).length
+const Whse_739_held = HeldLoads.filter(load => load.Wets === 739).length
+const Whse_749_held = HeldLoads.filter(load => load.Wets === 749).length
+const Whse_1025_held = HeldLoads.filter(load => load.Wets === 1025).length
+const Whse_1062_held = HeldLoads.filter(load => load.Wets === 1062).length
+const Whse_1070_held = HeldLoads.filter(load => load.Wets === 1070).length
+const Whse_1081_held = HeldLoads.filter(load => load.Wets === 1081).length
+const Whse_1093_held = HeldLoads.filter(load => load.Wets === 1093).length
+const Whse_1103_held = HeldLoads.filter(load => load.Wets === 1103).length
+const Whse_1114_held = HeldLoads.filter(load => load.Wets === 1114).length
+const Whse_1166_held = HeldLoads.filter(load => load.Wets === 1166).length
+const Whse_1174_held = HeldLoads.filter(load => load.Wets === 1174).length
+const Whse_1177_held = HeldLoads.filter(load => load.Wets === 1177).length
+const Whse_1199_held = HeldLoads.filter(load => load.Wets === 1199).length
+const Whse_1211_held = HeldLoads.filter(load => load.Wets === 1211).length
+const Whse_1214_held = HeldLoads.filter(load => load.Wets === 1214).length
+const Whse_1215_held = HeldLoads.filter(load => load.Wets === 1215).length
+const Whse_1236_held = HeldLoads.filter(load => load.Wets === 1236).length
+const Whse_1334_held = HeldLoads.filter(load => load.Wets === 1334).length
+const Whse_1415_held = HeldLoads.filter(load => load.Wets === 1415).length
