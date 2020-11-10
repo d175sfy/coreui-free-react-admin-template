946862f59e7b6c129ab8749cea1b1b71a68c6522
Eric V <d175sfy@costco.com>
2020-11-10 13:03:05 -0500

    toggle header working

--- src/components/DryAs400.js
+++ src/components/DryAs400.js
@@ -7,28 +7,60 @@ import "./DryAs400.css";
 const DryAs400 = () => {
   const [store, setStore] = useState(...StoreList)
   const [loadShown, setloadShown] = useState([])
+  const [showHeader, setshowHeader] = useState([])
 
 
   const toggleLoad = warehouse => {
-    console.log(warehouse)
+   // console.log(warehouse)
     //slice method to return selected elements as new array object
     const shownState = loadShown.slice()
+    //console.log(shownState)
     //indexOf to search array for specified item
     const index = shownState.indexOf(warehouse)
+    //console.log(index)
     
       if(index >= 0) {
         //splice // adds/removes item
         // 1 means remove 1 item
         // remove one item if found
         shownState.splice(index, 1)
-        setloadShown(loadShown)
+        setloadShown(shownState)
+        
       
       }else {
         shownState.push(warehouse)
         setloadShown(shownState)
       }
-      return StoreList
+
+      headerShow(warehouse)
+     // console.log(showHeader)
     }
+// function for showing header
+
+    const headerShow = warehouse => {
+      console.log(warehouse)
+      //slice method to return selected elements as new array object
+      const shownHeader = showHeader.slice()
+     // console.log(shownHeader)
+      //indexOf to search array for specified item
+      const index = shownHeader.indexOf(warehouse)
+      //console.log(index)
+      
+        if(index >= 0) {
+          //splice // adds/removes item
+          // 1 means remove 1 item
+          // remove one item if found
+          shownHeader.splice(index, 1)
+          setshowHeader(shownHeader)
+          
+        
+        }else {
+          shownHeader.push(warehouse)
+          setshowHeader(shownHeader)
+        }
+  
+      
+      }
   
   return (
     <div className="main-container">
@@ -45,7 +77,7 @@ const DryAs400 = () => {
 
             <div className="store">{whse.warehouse}</div>
           </div>
-          <div className="load-header">
+          <div className={showHeader == whse.warehouse ? "load-header" : "load-header-hidden"}>
                   <div className="heading">Time</div>
                   <div className="heading">Date</div>
                   <div className="heading">Status</div>
@@ -56,8 +88,8 @@ const DryAs400 = () => {
           {loadShown.includes(whse.warehouse) && (
             As400.filter((load) => load.warehouse === whse.warehouse).map(
             (load, i) => (
-              <div className={"load-container" + (loadShown ? 'load-container-open':'')} key={i}>
-                <div className="load">{`${i + 1}.`}{load.time}</div>
+              <div className={loadShown === whse.warehouse? 'load-container-hidden' : 'load-container'} key={i}>
+                <div className="load">{`${i + 1}.`}{load.time}</div> 
                 <div className="load">{load.date}</div>
                 <div className="load">{load.status}</div>
                 <div className="load">{load.loadNumber}</div>
