37befdb6c2177d1850dc1da9e68b2af1795cf495
Eric V <d175sfy@costco.com>
2020-11-14 15:00:32 -0500

    added bordered list to DriverAdmin page

--- src/views/dispatch/drivers/DriverAdmin.js
+++ src/views/dispatch/drivers/DriverAdmin.js
@@ -1,12 +1,62 @@
 import React from 'react'
-import TheHeader from '../../../containers/TheHeader'
-
+import {
+    CBadge,
+    CCard,
+    CCardBody,
+    CCardHeader,
+    CCol,
+    CDataTable,
+    CRow
+  } from '@coreui/react'
+  
+  import DriverList from '../../../utils/drivers/Driverlist.js'
+  
+  const getBadge = status => {
+    switch (status) {
+      case 'Active': return 'success'
+      case 'Inactive': return 'secondary'
+      case 'Pending': return 'warning'
+      case 'Banned': return 'danger'
+      default: return 'primary'
+    }
+  }
+  const fields = ['id','name','tractor', 'shift']
 function DriverAdmin() {
     return (
-        <div>
-            <TheHeader/>
-        </div>
+        <>
+        <CRow>
+          <CCol xs="12" lg="12">
+            <CCard>
+              <CCardHeader>
+                Depot 175 Fleet Drivers
+              </CCardHeader>
+              <CCardBody>
+              <CDataTable
+                items={DriverList}
+                fields={fields}
+                bordered
+                itemsPerPage={14}
+                pagination
+                scopedSlots = {{
+                  'status':
+                    (item)=>(
+                      <td>
+                        <CBadge color={getBadge(item.status)}>
+                          {item.status}
+                        </CBadge>
+                      </td>
+                    )
+  
+                }}
+              />
+              </CCardBody>
+            </CCard>
+          </CCol>
+  
+        </CRow>
+  
+       
+      </>
     )
-}
-
+  }
 export default DriverAdmin
