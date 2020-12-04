d7ce6c19a3e98067f6dd3396cc9e291cc2949c05
Eric V <d175sfy@costco.com>
2020-12-04 07:50:15 -0500

    driverList formatting

---
+++ src/components/createDriver/DriverScheduleComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './DriverScheduleComponent.css'
+
+function DriverScheduleComponent() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default DriverScheduleComponent
